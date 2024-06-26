d998b562d8e3015e3368b79a8f7a671ad07984d0
Ashish Mohapatra <78657461+ashishmohapatra240@users.noreply.github.com>
2024-06-26 14:52:18 +0530

    Time: 5 ms (32.24%), Space: 10.3 MB (14.09%) - LeetHub

--- 1693-sum-of-all-odd-length-subarrays/1693-sum-of-all-odd-length-subarrays.cpp
+++ 1693-sum-of-all-odd-length-subarrays/1693-sum-of-all-odd-length-subarrays.cpp
@@ -1,22 +1,22 @@
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
     int sumOddLengthSubarrays(vector<int>& arr) {
         int sum = 0;
         int n = arr.size();
-        
+        vector<int> prefixSum(n + 1, 0);
+        for (int i = 0; i < n; i++) {
+            prefixSum[i + 1] = prefixSum[i] + arr[i];
+        }
+        int totalSum = 0;
         for (int i = 0; i < n; i++) {
             for (int j = i; j < n; j++) {
                 if ((j - i + 1) % 2 == 1) {
-                    for (int k = i; k <= j; k++) {
-                        sum += arr[k];
-                    }
+
+                    totalSum += prefixSum[j + 1] - prefixSum[i];
                 }
             }
         }
-        
-        return sum;
+
+        return totalSum;
     }
 };
