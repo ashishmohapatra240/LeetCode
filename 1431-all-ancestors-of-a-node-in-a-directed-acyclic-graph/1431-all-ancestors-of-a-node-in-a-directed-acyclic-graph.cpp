b0743426fa5d5822cc698766a2ad143dc6ad99b3
Ashish Mohapatra <78657461+ashishmohapatra240@users.noreply.github.com>
2024-06-29 19:25:03 +0530

    Time: 210 ms (44.46%), Space: 73.7 MB (64.43%) - LeetHub

---
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    void DFS(int u, unordered_map<int, vector<int>>& adj,
+             vector<bool>& visited) {
+        visited[u] = true;
+        for (int& v : adj[u]) {
+            if (visited[v] != true) {
+                DFS(v, adj, visited);
+            }
+        }
+    }
+    vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
+        vector<vector<int>> result(n);
+        unordered_map<int, vector<int>> adj;
+
+        for (vector<int>& vec : edges) {
+            int u = vec[0];
+            int v = vec[1];
+
+            adj[v].push_back(u);
+        }
+
+        for (int u = 0; u < n; u++) {
+            vector<int> ancestors;
+            vector<bool> visited(n, false);
+            DFS(u, adj, visited);
+
+            for (int i = 0; i < n; i++) {
+                if(i==u) continue;
+                if (visited[i] == true) {
+                    ancestors.push_back(i);
+                }
+            }
+result[u] = ancestors;        }
+        return result;
+    }
+};
