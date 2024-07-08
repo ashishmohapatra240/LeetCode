64e0a457479af98367261f7668b16bdbdc333e28
Ashish Mohapatra <78657461+ashishmohapatra240@users.noreply.github.com>
2024-07-08 19:50:09 +0530

    Time: 127 ms (16.67%), Space: 76.1 MB (58.33%) - LeetHub

--- 3264-maximum-points-after-enemy-battles/3264-maximum-points-after-enemy-battles.cpp
+++ 3264-maximum-points-after-enemy-battles/3264-maximum-points-after-enemy-battles.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     long long maximumPoints(vector<int>& enemyEnergies, int currentEnergy) {
         sort(enemyEnergies.begin(), enemyEnergies.end());
-        int minEnergy =enemyEnergies[0];
-        if(minEnergy>currentEnergy) return 0;
-        long long points = 0;
+        int minEnergy = enemyEnergies[0];
+        if (minEnergy > currentEnergy)
+            return 0;
+        long long sum = 0;
 
-        for(auto energy: enemyEnergies)
-        points+=energy;
-        points-=minEnergy;
-        points+=currentEnergy;
+        for (auto energy : enemyEnergies)
+            sum += energy;
+        sum -= minEnergy;
+        sum += currentEnergy;
 
-        return points/minEnergy;
-        
+        return sum / minEnergy;
     }
 };
